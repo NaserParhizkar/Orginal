d283b2b353fdbef958ff54934224c5e121ef8a38
naser.parhizkar.se@gmail.com <naser.parhizkar.se@gmail.com>
2024-01-30 15:56:08 +0330

    first commit

---
+++ Dependent/ServiceCollectionExtentions.cs
@@ -0,0 +1,39 @@
+﻿using Dependent.Components.Account;
+using Dependent.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dependent
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection AddDependentServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddCascadingAuthenticationState();
+            services.AddScoped<IdentityUserAccessor>();
+            services.AddScoped<IdentityRedirectManager>();
+            services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+                .AddIdentityCookies();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+
+            services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+            return services;
+        }
+    }
+}
